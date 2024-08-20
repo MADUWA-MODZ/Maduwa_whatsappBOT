c15f189d0e62ca3d0dc9b191f31ea385af6ac170
Madumal <106644082+MADUWA-MODZ@users.noreply.github.com>
2024-08-20 23:29:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG, || "https://i.ibb.co/jf0Vp9v/MADUWA-MODS1.jpg",
+ALIVE_MSG: process.envALIVE_MSG, || "Hello, I am YourName i am alive now!",
 };
